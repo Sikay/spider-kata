8a28e0971b71d8122996618cf8f3e354d3a656c5
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-07 16:15:06 +0200

    should_return_same_y_coordinate

--- php/src/Coordinate.php
+++ php/src/Coordinate.php
@@ -5,14 +5,21 @@ namespace SpiderKata;
 class Coordinate
 {
     private $x;
+    private $y;
 
     public function __construct(int $x, int $y)
     {
         $this->x = $x;
+        $this->y = $y;
     }
 
     public function x()
     {
         return $this->x;
+    }
+
+    public function y()
+    {
+        return $this->y;
     }
 }
