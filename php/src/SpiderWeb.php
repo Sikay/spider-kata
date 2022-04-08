38a4a0eac58b1413758c1e38885b8a6e4f60248e
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-08 12:29:16 +0200

    should_exceed_the_limit_x_coordinate green

--- php/src/SpiderWeb.php
+++ php/src/SpiderWeb.php
@@ -4,8 +4,11 @@ namespace SpiderKata;
 
 class SpiderWeb
 {
-    public function exceedLimit()
+    public function exceedLimit(Coordinate $coordinate): bool
     {
+        if ($coordinate->x() >= 5) {
+            return true;
+        }
         return false;
     }
 }
