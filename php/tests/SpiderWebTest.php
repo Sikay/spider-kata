e418d9d88b7674a2316f5ae88029f1b400b15ab6
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-08 13:34:10 +0200

    changed test to wait for an exception

--- php/tests/SpiderWebTest.php
+++ php/tests/SpiderWebTest.php
@@ -19,32 +19,36 @@ class SpiderWebTest extends TestCase
    /** @test */
    public function should_exceed_the_limit_x_coordinate()
    {
+        $this->expectException(\InvalidArgumentException::class);
         $coordinate = new Coordinate(5, 3);
         $spiderWeb = new SpiderWeb();
-        $this->assertTrue($spiderWeb->exceedLimit($coordinate));
+        $spiderWeb->exceedLimit($coordinate);
    }
 
    /** @test */
    public function should_exceed_the_limit_y_coordinate()
    {
+        $this->expectException(\InvalidArgumentException::class);
         $coordinate = new Coordinate(2, 6);
         $spiderWeb = new SpiderWeb();
-        $this->assertTrue($spiderWeb->exceedLimit($coordinate));
+        $spiderWeb->exceedLimit($coordinate);
    }
 
    /** @test */
    public function should_exceed_the_limit_x_with_minus_value_coordinate()
    {
+        $this->expectException(\InvalidArgumentException::class);
         $coordinate = new Coordinate(-1, 3);
         $spiderWeb = new SpiderWeb();
-        $this->assertTrue($spiderWeb->exceedLimit($coordinate));
+        $spiderWeb->exceedLimit($coordinate);
    }
 
    /** @test */
    public function should_exceed_the_limit_y_with_minus_value_coordinate()
    {
+        $this->expectException(\InvalidArgumentException::class);
         $coordinate = new Coordinate(1, -1);
         $spiderWeb = new SpiderWeb();
-        $this->assertTrue($spiderWeb->exceedLimit($coordinate));
+        $spiderWeb->exceedLimit($coordinate);
    }
 }
