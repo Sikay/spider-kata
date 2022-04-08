6e6e1431a5945b247b320fa6768a620fc6ddc92a
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-08 12:46:10 +0200

    refactor for more semantic

--- php/src/SpiderWeb.php
+++ php/src/SpiderWeb.php
@@ -4,9 +4,30 @@ namespace SpiderKata;
 
 class SpiderWeb
 {
+    private const MAX_HEIGHT = 5;
+    private const MAX_WITDH = 4;
+    private const MIN_HEIGHT = 0;
+    private const MIN_WITDH = 0;
+
     public function exceedLimit(Coordinate $coordinate): bool
     {
-        if ($coordinate->x() >= 5 || $coordinate->x() < 0 || $coordinate->y() >= 6 || $coordinate->y() < 0) {
+        if (self::exceedWidthLimit($coordinate->x()) || self::exceedHeightLimit($coordinate->y())) {
+            return true;
+        }
+        return false;
+    }
+
+    private static function exceedWidthLimit(int $width): bool
+    {
+        if ($width > self::MAX_WITDH || $width < self::MIN_WITDH) {
+            return true;
+        }
+        return false;
+    }
+
+    private static function exceedHeightLimit(int $height): bool
+    {
+        if ($height > self::MAX_HEIGHT || $height < self::MIN_HEIGHT) {
             return true;
         }
         return false;
